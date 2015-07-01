340e3eac8f62d9a45123cd2968b350821833e4d2
mtojar <mtojar@gmail.com>
2015-07-01 12:06:11 +0200

    Version 1 de random. Prueba de proyecto github

---
+++ random.js
@@ -0,0 +1,7 @@
+//Math.random() devuelve número aleatorio entre 0 y 1
+var numero = Math().ransom();
+
+if(numero <= 0)
+  console.log('\n' + numero + ' MENOR que 0,5 \n');
+else
+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
